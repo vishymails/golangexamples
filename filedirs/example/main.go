9ad2df871c733a50e1983d05f43b191a382c7f8e
vishymails <vishymails@gmail.com>
2020-09-19 15:20:26 +0530

    Add files via upload

---
+++ filedirs/example/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/vishymails/golangexamples/filedirs"
+
+func main() {
+	if err := filedirs.Operate(); err != nil {
+		panic(err)
+	}
+
+	if err := filedirs.CapitalizerExample(); err != nil {
+		panic(err)
+	}
+}
