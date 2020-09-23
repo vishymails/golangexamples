b7db4313c6a44a40f58990f338035eb7ddf2b75b
vishymails <vishymails@gmail.com>
2020-09-23 14:20:34 +0530

    Add files via upload

---
+++ DAY3/chan1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int, 10)
+	makeEvenNums(6, ch)
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	//fmt.Println(<-ch)
+}
+
+func makeEvenNums(count int, in chan<- int) {
+	for i := 0; i < count; i++ {
+		in <- 2 * i
+	}
+}
